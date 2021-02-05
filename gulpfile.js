1cd28278cbc9b02dd16fe12ed7f9e025d3bf491a
Dung Bui Ke <dungbuike@prtadminnoMacBook-Pro.local>
2021-02-05 17:35:12 +0900

    fix sass file: block__element code place && refractor file tree

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function() {
-    return gulp.src('./sass/**/*.scss')
+    return gulp.src('./sass/**/components.scss')
         .pipe(sass({outputStyle: 'expanded'}))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css/'));
 });
 
 gulp.task('sass:watch', function () {
-    gulp.watch('./sass/**/.scss', ['sass']);
+    gulp.watch('./sass/**/components.scss', ['sass']);
 });
